32dfed937d23891e45c4ab375525b4621b982a3c
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-02-06 19:00:38 -0700

    added comments table to seeds

---
+++ seeds/commentData.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { text: "this is an amazing recipe", date_created: "11/24/24", recipe_id: 1, user_id: 1 },
+  { text: "this is an amazing recipe", date_created: "9/24/24", recipe_id: 2, user_id: 1 },
+  { text: "this is an amazing recipe", date_created: "8/24/24", recipe_id: 3, user_id: 2 },
+  { text: "this is an amazing recipe", date_created: "10/24/24", recipe_id: 4, user_id: 3 },
+];
--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,7 @@ const { User, Comment, Ingredient, Recipe } = require("../models");
 
 const userData = require("./userData.json");
 const recipeData = require("./recipeData");
+const commentData = require("./commentData");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,18 +14,25 @@ const seedDatabase = async () => {
   });
 
   for (const recipe of recipeData) {
-
     const dbRecipe = await Recipe.create({
       ...recipe,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
 
-    for(const ingredient of recipe.ingredients){
+    for (const ingredient of recipe.ingredients) {
       const dbIngredient = await Ingredient.create({
         ...ingredient,
         recipe_id: dbRecipe.id,
       });
     }
+  }
+
+  for (const comment of commentData) {
+    const comments = await Comment.create({
+      ...comment,
+      user_id: comment.user_id,
+      recipe_id: comment.recipe_id,
+    });
   }
 
   process.exit(0);

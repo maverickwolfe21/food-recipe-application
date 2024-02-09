8bcd748bc18385b8833485c561ea4ade2c7df5e3
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-02-08 21:28:17 -0700

    upload recipe handlebars and js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,18 +1,14 @@
-//function to send request to log out the user
-// const userLogout = async () => {
-//   const response = await fetch("/api/users/logout", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//   });
+const logout = async () => {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
 
-//   if (response.ok) {
-//     document.location.replace("/"); //if successful load the homepage
-//   } else {
-//     alert("Failed to log out."); //if unsuccessful show alert
-//   }
-// };
-// //event listener for the logout button
-// const userLogoutButton = document.querySelector("#logout");
-// if (userLogoutButton) {
-//   userLogoutButton.addEventListener("click", userLogout);
-// }
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector("#logout").addEventListener("click", logout);

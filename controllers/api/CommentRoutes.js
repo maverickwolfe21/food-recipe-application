9bd7b7d29cf466584fc2ef19960b5f227a05983c
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-02-09 21:19:43 -0700

    comment functionality

--- controllers/api/CommentRoutes.js
+++ controllers/api/CommentRoutes.js
@@ -3,28 +3,28 @@ const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 // get request to pull comment data
-// router.get("/", async (req, res) => {
-//   try {
-//     const commentData = await Comment.findAll();
-//     res.status(200).json(commentData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// // GET a single recipe
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const commentData = await Comment.findByPk(req.params.id);
-//     if (!commentData) {
-//       res.status(404).json({ message: "No comment found with this id!" });
-//       return;
-//     }
-//     res.status(200).json(commentData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// GET a single recipe
+router.get("/:id", async (req, res) => {
+  try {
+    const commentData = await Comment.findByPk(req.params.id);
+    if (!commentData) {
+      res.status(404).json({ message: "No comment found with this id!" });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post("/", withAuth, async (req, res) => {
   try {
